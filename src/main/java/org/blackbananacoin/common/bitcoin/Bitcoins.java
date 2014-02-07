e8fdcd9d11a9ad68f7aa269584f33fb1ba4bb636
y12studio <y12studio@gmail.com>
2014-02-07 19:27:25 +0800

    new wallet private format support

--- src/main/java/org/blackbananacoin/common/bitcoin/Bitcoins.java
+++ src/main/java/org/blackbananacoin/common/bitcoin/Bitcoins.java
@@ -4,13 +4,18 @@ import static com.google.common.base.Preconditions.checkNotNull;
 
 import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import com.google.bitcoin.core.ECKey;
+import com.google.bitcoin.core.NetworkParameters;
 import com.google.bitcoin.core.Utils;
 
 public class Bitcoins {
 
 	public static final long COIN = 100000000L;;
+	public static final SimpleDateFormat FMDateKeyContent = new SimpleDateFormat(
+			"yyyy-MM-dd'T'HH:mm:ss'Z'");
 
 	/**
 	 * @param addr
@@ -30,5 +35,19 @@ public class Bitcoins {
 		ECKey eckey = new ECKey(new BigInteger(1, Utils.doubleDigest(seed
 				.getBytes("UTF-8"))));
 		return eckey;
+	}
+
+	public static String walletPrivateKeyFormat(ECKey k,
+			NetworkParameters params, int hrAgo) {
+		// wallet key save to /mnt/sdcard/datelabel.key
+		// private key file content
+		// BitcoinxxxxxxxxxxxxxxxAddress 2014-01-28T09:13:54Z
+		long time = new Date().getTime();
+		long createTime = time - hrAgo * 60 * 60 * 1000;
+		String timeInKey = FMDateKeyContent.format(new Date(createTime));
+		String keyContent = "# KEEP YOUR PRIVATE KEYS SAFE !\n"
+				+ k.getPrivateKeyEncoded(params).toString() + " " + timeInKey
+				+ "\n# End of private keys";
+		return keyContent;
 	}
 }
